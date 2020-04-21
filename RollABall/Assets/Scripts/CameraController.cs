c5a5464eef940f8faa1c51566bfb4a3cc845d9a3
HelenaGH <47184541+HelenaGH@users.noreply.github.com>
2020-04-21 17:27:20 +0200

    RollingStonesI
    none

---
+++ RollABall/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject player;
+	private Vector3 pomak;
+
+	
+	void Start () 
+	{
+		pomak = transform.position - player.transform.position;
+	}
+	
+	
+	void LateUpdate () 
+	{
+		transform.position = player.transform.position + pomak;
+	}
+}
---
+++ RollABall/Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour 
+{
+
+	// deklaracija varijable za brzinu i RigidBody komponentu
+	public Text countText;
+	public float speed;
+	private int zbroj;
+	private Rigidbody rb;
+	void Start () 
+	{
+		// dohvaćanje komponente
+		rb = GetComponent<Rigidbody>();
+		zbroj = 0;
+		SetCountText();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		// dohvaćanje vrijednosti za osi
+		float movementHorizontal = Input.GetAxis("Horizontal");
+		float movementVertical = Input.GetAxis("Vertical");
+
+		// postavljanje vektora
+		Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementVertical);
+
+		// dodavanje sile na objekt
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		// provjera tag-a drugog objekta
+		if (other.gameObject.CompareTag("Pick Up"))
+		{
+			// objekt se deaktivira
+			other.gameObject.SetActive(false);
+			// nakon svakog skupljenog objekta rezultat se povećava za 1
+			zbroj += 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Rezultat" + zbroj.ToString();
+	}
+}
